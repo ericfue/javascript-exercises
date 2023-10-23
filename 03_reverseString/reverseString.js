95dcd38aff7add169cc257a2c9b8b3df594c2994
ericfue <107294764+ericfue@users.noreply.github.com>
2023-10-22 21:48:39 -0400

    Add solutions for exercises 1, 2, and 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    reversed = ""
+    len = string.length;
+    for (let i = (len - 1); i > -1; i--) {
+        letter = string.charAt(i);
+        reversed += letter;
+    }
+    return reversed;
 };
 
 // Do not edit below this line
