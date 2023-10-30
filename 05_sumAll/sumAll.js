cb1c28a6409e4f309fd8313965cdd1f431d1aa2b
ericfue <107294764+ericfue@users.noreply.github.com>
2023-10-30 00:26:49 -0400

    Add solution for exercise 5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let sum = 0;
+    if (x < 0 || y < 0 || typeof x !== "number" || typeof y !== "number") {
+        return "ERROR";
+    } else if ((x - y) < 0) {
+        while (x <= y) {
+            sum += x;
+            x++;
+        }
+    } else {
+        while (x >= y) {
+            sum += x;
+            x--;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
