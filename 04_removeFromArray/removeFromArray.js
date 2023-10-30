a60ca43de9b14528cfbe3944ac6ca4e784558631
ericfue <107294764+ericfue@users.noreply.github.com>
2023-10-29 23:56:10 -0400

    Add solution for exercise 4.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+    const myArray = Array.from(array);
+    for (let i = 0; i < myArray.length; i++) {
+        for (let j = 0; j < toRemove.length; j++) {
+            if (myArray[i] === toRemove[j]) {
+                myArray.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
